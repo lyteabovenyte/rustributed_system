7f39c146cccbb2ee4cbc789782d84011bb312bbd
lyteabovenyte <lyteabovenyte@gmail.com>
2025-07-28 03:36:36 +0330

    basic motion of the system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::StdoutLock;
+
+use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Message {
+    src: String,
+    #[serde(rename = "dest")]
+    dst: String,
+    body: Body,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Body {
+    #[serde(rename = "msg_id")]
+    id: Option<usize>,
+    in_reply_to: Option<usize>,
+    #[serde(flatten)]
+    payload: Payload,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Echo { echo: String },
+    EchoOk { echo: String },
+}
+
+struct EchoNode {
+    id: usize,
+}
+
+impl EchoNode {
+    pub fn step(
+        &mut self,
+        input: Message,
+        output: &mut serde_json::Serializer<StdoutLock>,
+    ) -> Result<()> {
+        match input.body.payload {
+            Payload::Echo { echo } => {
+                let reply = Message {
+                    src: input.src,
+                    dst: input.dst,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::EchoOk { echo },
+                    },
+                };
+                reply
+                    .serialize(output)
+                    .context("seralize reponse to echo")?;
+                self.id += 1;
+            }
+            Payload::EchoOk { .. } => {}
+        }
+        Ok(())
+    }
+}
+fn main() -> Result<()> {
+    let stdin = std::io::stdin().lock();
+    let inputs = serde_json::Deserializer::from_reader(stdin).into_iter::<Message>();
+
+    let stdout = std::io::stdout().lock();
+    let mut output = serde_json::Serializer::new(stdout);
+
+    let mut state = EchoNode { id: 0 };
+
+    for input in inputs {
+        let input = input.context("input from STDIN could not be deserialized.")?;
+        state
+            .step(input, &mut output)
+            .context("Node step function failed")?;
+    }
+
+    Ok(())
 }
